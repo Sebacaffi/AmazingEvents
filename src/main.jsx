3ba2dfc16755112781b9e449c60d547303f51afe
SebaCaffi <sebastian.caffi@gmail.com>
2023-07-26 16:51:55 -0400

    entrega final

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import StateProvider from './store/StateProvider.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StateProvider>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </StateProvider>
   </React.StrictMode>,
 )
